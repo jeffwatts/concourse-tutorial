06af57887f6d205dc87680b8677ceff187f91c70
Jeff Watts <jwatts@periscopedata.com>
2018-03-22 21:48:27 -0700

    Filling in set-up-props.sh

--- jeffpipeline/set-up-props.sh
+++ jeffpipeline/set-up-props.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 echo "Trying to set key-value properties"
+
+propsdir = "keyval"
+props_file = "${propsdir}/keyval.properties"
+if [ -d "$propsdir" ]; then
+    touch "$props_file"
+    echo "Setting values for next job in ${props_file}"
+    echo "Adding BUILD_VERSION=1.0.0-abcdef"
+    echo "BUILD_VERSION=1.0.0-abcdef" >> "${props_file}"
+else
+    echo "Could not find dir ${propsdir}"
+    ls -lR .
+fi
