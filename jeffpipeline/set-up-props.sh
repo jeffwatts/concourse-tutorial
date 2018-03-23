27ca66c2f854b47a0ba0b5e8ad2cab2f38be9ed8
Jeff Watts <jwatts@periscopedata.com>
2018-03-22 22:18:00 -0700

    Making BUILD_VERSION more dynamic

--- jeffpipeline/set-up-props.sh
+++ jeffpipeline/set-up-props.sh
@@ -7,8 +7,9 @@ props_file="${propsdir}/keyval.properties"
 if [ -d "$propsdir" ]; then
     touch "$props_file"
     echo "Setting values for next job in ${props_file}"
-    echo "Adding BUILD_VERSION=1.0.0-abcdef"
-    echo "BUILD_VERSION=1.0.0-abcdef" >> "${props_file}"
+    cur_date=`date +%s`
+    echo "Adding BUILD_VERSION=1.0.0-${cur_date}"
+    echo "BUILD_VERSION=1.0.0-abcdef-${cur_date}" >> "${props_file}"
 else
     echo "Could not find dir ${propsdir}"
     ls -lR .
